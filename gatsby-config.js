e22bb7a5d44fce5b5c483a4f567db684aabd821c
Excellence Oyeniran <oyeniranexcellenced@gmail.com>
2023-05-05 18:20:35 +0100

    trigger commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sass",
     {
-      // keep as first gatsby-source-filesystem plugin for gatsby image support
+      // keep as first gatsby-source-filesystem plugin for gatsby image support.
       resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/static/img`,
